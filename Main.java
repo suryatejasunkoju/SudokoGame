431649f188931545d62cf417834edf4be2be1b8a
Suryateja Sunkoju <sunkojusuryateja@gmail.com>
2022-10-25 23:37:57 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main
+{
+    public static void main(String[] args) 
+    {
+        // Board b=new Board();
+        // int[][] randomMatrix=b.getFinalBoard(); 
+        Solve s=new Solve();
+        boolean isSolvable=s.solveBoard(randomMatrix);   
+        // if()
+    }
+}
